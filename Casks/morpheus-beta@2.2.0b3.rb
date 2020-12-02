9a8f979ae637a2de0337a6f12ae162a325fe9c6b
Diego Jahn <jahndiego@mailbox.org>
2020-12-02 17:55:35 +0100

    Add Morpheus.app v2.2.0b3

---
+++ Casks/morpheus-beta@2.2.0b3.rb
@@ -0,0 +1,28 @@
+cask "morpheus-beta@2.2.0b3" do
+    version "2.2.0_beta3"
+    sha256 "20ba4b5860d02dbdf703f15c9d7828162f8a9d5eba50480f0ac04fde9be7a1c3"
+  
+    # imc.zih.tu-dresden.de/morpheus/ was verified as official when first introduced to the cask
+    url "https://imc.zih.tu-dresden.de/morpheus/packages/mac/Morpheus-#{version}.dmg"
+    appcast "https://morpheus.gitlab.io/download/"
+    name "Morpheus"
+    desc "Modelling environment for multi-cellular systems biology"
+    homepage "https://morpheus.gitlab.io/"
+  
+    conflicts_with cask: "morpheus"
+  
+    app "Morpheus.app"
+  
+    zap trash: [
+      "~/Library/Application Support/data/Morpheus",
+      "~/Library/Application Support/Morpheus",
+      "~/Library/Application Support/CrashReporter/morpheus_*.plist",
+      "~/Library/Caches/Morpheus",
+      "~/Library/Preferences/morpheus-gui.plist",
+      "~/Library/Preferences/io.gitlab.morpheus.morpheus.plist",
+      "~/Library/Preferences/de.tu-dresden.Morpheus.plist",
+      "~/Library/Preferences/org.morpheus.Morpheus.plist",
+      "~/Library/Saved Application State/de.tu-dresden.Morpheus.savedState",
+      "~/Library/Saved Application State/io.gitlab.morpheus.morpheus.savedState",
+    ]
+  end
